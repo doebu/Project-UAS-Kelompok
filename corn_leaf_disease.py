ce02e37ef034a7f995a09319e13b12376b1642b8
Gusmang Adi <cybermoon3@gmail.com>
2024-12-08 22:37:58 +0800

    Nambahin preprocessing + folder untuk hasil testing

--- corn_leaf_disease.py
+++ corn_leaf_disease.py
@@ -4,31 +4,37 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 
-dataset_dir = r'D:\1_Kuliah Gusmang\Semester_3\Kuliah\1_Pengolahan_Citra_Digital\Project UAS Kelompok\Dataset'
+dataset_dir = r'D:\1_Kuliah Gusmang\Semester_3\Kuliah\1_Pengolahan_Citra_Digital\Project-UAS-Kelompok\Dataset'
 
 images = []
 labels = []
 
 label_map = {'Healthy': 0, 'Blight': 1, 'Common_Rust': 2, 'Gray_Leaf_Spot': 3,}
 
-for label, label_idx in label_map.items():
-    label_dir = os.path.join(dataset_dir, label)
+def load_data(dataset_dir, label_map):
+    images = []
+    labels = []
+    for label, label_idx in label_map.items():
+        label_dir = os.path.join(dataset_dir, label)
 
-    for filename in os.listdir(label_dir):
-        img_path = os.path.join(label_dir, filename)
+        for filename in os.listdir(label_dir):
+            img_path = os.path.join(label_dir, filename)
 
-        img = cv2.imread(img_path)
+            img = cv2.imread(img_path)
 
-        img = cv2.resize(img, (128, 128))
+            img = cv2.resize(img, (128, 128))
 
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        images.append(img_rgb)
-        labels.append(label_idx)
+            images.append(img_rgb)
+            labels.append(label_idx)
 
+    images = np.array(images)
+    labels = np.array(labels)
 
-images = np.array(images)
-labels = np.array(labels)
+    return images, labels
+
+images, labels = load_data(dataset_dir, label_map)
 
 fig, axes = plt.subplots(1, 4, figsize=(20, 5))
 
@@ -44,4 +50,57 @@ for i, (label, label_idx) in enumerate(label_map.items()):
     ax.axis('off')
 
 plt.tight_layout()
-plt.show()
+plt.savefig('Plot Hasil Testing/baca_awal.png', bbox_inches='tight')
+# plt.show()
+
+def preprocess_images(images):
+    processed_images = []
+
+    for img in images:
+        # img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        img_ycrcb = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+        y, cr, cb = cv2.split(img_ycrcb)
+        
+        # Equalisasi histogram pada saluran Y
+        y_eq = cv2.equalizeHist(y)
+        img_ycrcb_eq = cv2.merge([y_eq, cr, cb])
+
+        # Kembali ke RGB
+        img_rgb = cv2.cvtColor(img_ycrcb_eq, cv2.COLOR_YCrCb2RGB)
+
+        # Normalisasi
+        img_normalized = img_rgb / 255.0
+        processed_images.append(img_normalized)
+
+        # img_blur = cv2.GaussianBlur(img, (3, 3), 0)
+
+        # img_eq = cv2.equalizeHist(img_blur[:, :, 0])
+        # img_eq = cv2.merge([img_eq, cv2.equalizeHist(img_blur[:, :, 1]), cv2.equalizeHist(img_blur[:, :, 2])])
+
+        # img_normalized = img_eq / 255.0
+
+        # processed_image.append(img_normalized)
+
+    return np.array(processed_images)
+
+images, labels = load_data(dataset_dir, label_map)
+
+processed_images = preprocess_images(images)
+
+fig, axes = plt.subplots(1, 4, figsize=(20, 5))
+
+for i, (label, label_idx) in enumerate(label_map.items()):
+    
+    idxs = np.where(labels == label_idx)[0]
+    
+    img = processed_images[idxs[0]]
+    
+    ax = axes[i]
+    ax.imshow(img)
+    ax.set_title(f"Label: {label}")
+    ax.axis('off')
+
+plt.tight_layout()
+plt.savefig('Plot Hasil Testing/preprocessing.png', bbox_inches='tight')
+# plt.show()
